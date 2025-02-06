fb12489ad908a7001b7280200662eba8bee4b8af
susamogus <a5457789889409@gmail.com>
2025-02-06 20:52:32 +0800

    Added sync script

---
+++ sync_config_files.zsh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+
+cp -r ~/.config/hypr ./
+cp -r ~/.config/rofi ./
+cp -r ~/.config/waybar ./
+echo "Config files synced."
--- waybar/nsfw_wallpapers.zsh
+++ waybar/nsfw_wallpapers.zsh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 
-nsfw_wallpaper_path=~/Pictures/nsfw_wallpaper
+nsfw_wallpaper_path=~/Pictures/ftnr/ebi/nsfw_wallpaper
 
 mpvpaper -p -n 10 DP-1 ${nsfw_wallpaper_path}
